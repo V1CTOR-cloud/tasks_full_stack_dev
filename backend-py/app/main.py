529b12bcb27de2b9accf284a333980ccf70fff44
Víctor Martínez Zapata <latipicacuenta89@gmail.com>
2026-09-03 11:18:41 +0200

    CORS

--- backend-py/app/main.py
+++ backend-py/app/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import user, auth, color
 from app.routers.project import router as project_router
 from app.routers.team import router as team_router
 from app.routers.task import router as tasks_router
 
-app = FastAPI(
-    title="tasks_full_stack_dev",
-    version="1.0.0"
+app = FastAPI(title="tasks_full_stack_dev", version="1.0.0")
+
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:5173",
+    "https://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user.router)
